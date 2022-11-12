3d374df22f21b661dd6e17dbc54ffc0ccce9fe84
კრიპტოლუქსი <73051326+Ghonghito@users.noreply.github.com>
2022-11-12 22:25:28 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from 'components/Header/Header';
 import Main from 'pages/Main'
 import CoinPage from 'pages/CoinPage'
 
 function App() {
+
+  useEffect(() => {
+    const darkModeSet = localStorage.getItem('darkMode')
+    if (darkModeSet === null) {
+      localStorage.setItem('darkMode', 'dark')
+      document.documentElement.classList.add('dark')
+    } else if (darkModeSet === 'light') {
+      localStorage.setItem('darkMode', 'dark')
+      document.documentElement.classList.add('dark')
+    } else if (darkModeSet === 'dark') {
+      localStorage.setItem('darkMode', 'light')
+      document.documentElement.classList.remove('dark')
+    }
+  }, [])
+
   return (
     <div className='duration-200 bg-whiteBackground dark:bg-zinc-900 min-h-screen'>
       <BrowserRouter>
